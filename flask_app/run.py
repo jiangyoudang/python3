569de08e915e753e954ab4474c07af1608b3d663
cong <893101471@qq.com>
2015-03-12 16:16:48 -0700

    Set apart some web apps

--- flask_app/run.py
+++
@@ -1,4 +0,0 @@
-from flask_app.app1 import app
-
-
-app.run(debug=True)
--- flaskapp_485/schema.sql
+++
@@ -1,6 +0,0 @@
-create table users (
-    id          integer primary key autoincrement not null,
-    name        text,
-    description   text,
-    email       text
-);
